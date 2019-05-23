e5c01d6c486afa9526dff44c1a5a6bc05cae55ce
dev_johsire <josiah5115@gmail.com>
2019-05-23 08:04:00 -0600

    Consume API endpoint uisng GET

--- scripts.js
+++ scripts.js
@@ -6,6 +6,16 @@ request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
 
 request.onload = function() {
   // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      // Log each movie's title
+      console.log(movie.title);
+    });
+  } else {
+    console.log("error");
+  }
 };
 
 // Send request
